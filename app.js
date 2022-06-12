0d28b20ab8a8de18159d723914e0707838f43f77
developerWH <devwheelhouse@gmail.com>
2022-06-12 10:06:08 +0530

    cors fix

--- app.js
+++ app.js
@@ -8,14 +8,15 @@ import cors from 'cors';
 var app = express();
 
 let allowCrossDomain = (req, res, next) => {
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  // res.header("Access-Control-Allow-Credentials", true);
+  // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "*");
 
   res.header(
     "Access-Control-Allow-Headers",
     "Origin,X-Requested-With,Content-Type,Accept,Authorization"
   );
-  res.header("Access-Control-Allow-Credentials", true);
+  // res.header("Access-Control-Allow-Credentials", true);
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", "PUT,POST,DELETE,PATCH");
     return res.status(200).json({});
